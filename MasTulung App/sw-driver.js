95f77facd15ac2582110d9b1759117c231eba756
BungRiski <hilmylmsh@gmail.com>
2026-04-10 18:53:05 +0700

    Update and rename sw.js to sw-driver.js

---
+++ MasTulung App/sw-driver.js
@@ -0,0 +1,28 @@
+// Service Worker - TulangTulung Driver App
+const CACHE_NAME = 'ttg-driver-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/driver.html', '/manifest-driver.json', '/logo-driver.png']);
+    }).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME && k.startsWith('ttg-driver')).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
