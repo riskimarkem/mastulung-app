8b8d15a9d22be759fe70a7a3cd8df0d0f44bc8b4
BungRiski <hilmylmsh@gmail.com>
2026-07-11 08:25:05 +0700

    Add files via upload

---
+++ MasTulung App/sw-resto.js
@@ -0,0 +1,28 @@
+// Service Worker - TulangTulung Resto App
+const CACHE_NAME = 'ttg-resto-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/resto.html', '/manifest-resto.json', '/logo-resto.png']);
+    }).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME && k.startsWith('ttg-resto')).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
